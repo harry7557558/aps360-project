062c4b6cc21b9411736dd21ef3c920b54898f120
castiblancoj <83900364+castiblancoj@users.noreply.github.com>
2024-02-23 00:21:50 -0500

    added DownscaleBlock

--- discriminator_model.py
+++ discriminator_model.py
@@ -4,5 +4,17 @@ import torch.nn as nn
 def conv3s(nin, nout, stride):
     return nn.Conv2d(nin, nout, 3, stride=stride, padding=1)
 
-def lrelu(x, slope, inplace=True):
-    return nn.functional.leaky_relu(x, slope, inplace=inplace)
+class DownscaleBlock(nn.Module):
+    def __init__(self, n, slope):
+        super(DownscaleBlock, self).__init__()
+        self.conv1 = conv3s(n, n, 2)
+        self.bn1 = nn.BatchNorm2d(n)
+        self.lrelu1 = nn.LeakyReLU(slope, inplace=True)
+        
+        self.conv2 = conv3s(n, n, 1)
+        self.bn2 = nn.BatchNorm2d(n)
+        self.lrelu2 = nn.LeakyReLU(slope, inplace=True)
+        
+    def forward(self, x):
+        x = self.lrelu1(self.bn1(self.conv1(x)))
+        return self.lrelu2(self.bn2(self.conv2(x)))

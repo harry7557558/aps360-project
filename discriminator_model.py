e956bc603df1ca6123664761880b9b0081a58833
castiblancoj <83900364+castiblancoj@users.noreply.github.com>
2024-02-23 20:06:05 -0500

    Corrected errors and added a test

--- discriminator_model.py
+++ discriminator_model.py
@@ -20,22 +20,22 @@ class DownscaleBlock(nn.Module):
         return self.lrelu2(self.bn2(self.conv2(x)))
 
 class DiscriminatorModel(nn.Module):
-    def __init__(self, n_in, n_out, n_hidden, slope):
-        super(DiscriminatorModel, self).__init()
+    def __init__(self, n_in, n_hidden, slope):
+        super(DiscriminatorModel, self).__init__()
         self.convi = conv3s(n_in, n_hidden, 1)
         self.lrelu1 = nn.LeakyReLU(slope, inplace=True)
         
         self.downscale = nn.Sequential(
-            DownscaleBlock(n_hidden),
-            DownscaleBlock(2*n_hidden),
-            DownscaleBlock(4*n_hidden),
+            DownscaleBlock(n_hidden, slope),
+            DownscaleBlock(2*n_hidden, slope),
+            DownscaleBlock(4*n_hidden, slope),
             conv3s(8*n_hidden, 8*n_hidden, 2),
             nn.BatchNorm2d(8*n_hidden),
             nn.LeakyReLU(slope, inplace=True)
         )
         
         self.ampl = nn.AdaptiveMaxPool2d((4,4))
-        self.flatten = nn.Flatten(0)
+        self.flatten = nn.Flatten()
         self.dense1 = nn.Linear(128*n_hidden, 32*n_hidden)
         self.lrelu2 = nn.LeakyReLU(slope, inplace=True)
         self.dense2 = nn.Linear(32*n_hidden, 1)
@@ -48,3 +48,14 @@ class DiscriminatorModel(nn.Module):
         xc1 = self.lrelu2(self.dense1(xf))
         xc2 = self.sigmoid(self.dense2(xc1))
         return xc2
+
+if __name__ == "__main__":
+    
+    x = torch.randn(8, 3, 64, 64)
+    
+    slope = 0.01
+    
+    model = DiscriminatorModel(3, 32, slope)
+    with torch.no_grad():
+        y = model(x)
+    print(y.shape)
